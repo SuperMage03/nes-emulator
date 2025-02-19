91f69434a46a635ea049e6d5654307d43a74af8f
Eston Li <liyuelin2004@gmail.com>
2025-02-19 14:39:40 +0100

    Finished laying out the ppu bus data addressing

--- src/ppu-bus.cpp
+++ src/ppu-bus.cpp
@@ -5,21 +5,21 @@ PPUBUS::PPUBUS(RP2C02& ppu, MemoryUnit& name_table, MemoryUnit& palette_table, s
     ppu_.connectBUS(this);
 }
 
-
 uint8_t PPUBUS::readBusData(const uint16_t& address) const {
     // Retrieve pattern table data from the Cartridge
     if ((0x0000 <= address) && (address <= 0x1FFF)) {
         return cartridge_->readChrMem(address);
     }
     // Retrieve name table data from the Name Table
-    if ((0x2000 <= address) && (address <= 0x2FFF)) {
-        return name_table_.read(address - 0x2000);
+    if ((0x2000 <= address) && (address <= 0x3EFF)) {
+        return name_table_.read((address - 0x2000) % 0x1000);
     }
     // Retrieve palette data from the Palette Table
     if ((0x3F00 <= address) && (address <= 0x3FFF)) {
-        return palette_table_.read(address - 0x3F00);
+        return palette_table_.read((address - 0x3F00) % 0x0020);
     }
-    return 0;
+    // Retrieve mirror of 0x0000 to 0x3FFF
+    return readBusData(address - 0x4000);
 }
 
 bool PPUBUS::writeBusData(const uint16_t& address, const uint8_t& data) {
@@ -28,12 +28,13 @@ bool PPUBUS::writeBusData(const uint16_t& address, const uint8_t& data) {
         return cartridge_->writeToChrMem(address, data);
     }
     // Write data to the Name Table
-    if ((0x2000 <= address) && (address <= 0x2FFF)) {
-        return name_table_.write(address - 0x2000, data);
+    if ((0x2000 <= address) && (address <= 0x3EFF)) {
+        return name_table_.write((address - 0x2000) % 0x1000, data);
     }
     // Write data to the Palette Table
     if ((0x3F00 <= address) && (address <= 0x3FFF)) {
-        return palette_table_.write(address - 0x3F00, data);
+        return palette_table_.write((address - 0x3F00) % 0x0020, data);
     }
-    return false;
+    // Write data to the mirror of 0x0000 to 0x3FFF
+    return writeBusData(address - 0x4000, data);
 }
