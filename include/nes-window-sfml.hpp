e3ba0e8fb4d9000e0317b56129cdf35e5d65be43
Eston Li <liyuelin2004@gmail.com>
2025-04-15 17:29:39 -0400

    Added Destructor for nes-window-sfml

--- include/nes-window-sfml.hpp
+++ include/nes-window-sfml.hpp
@@ -13,6 +13,7 @@
 class NESWindowSFML : public NESWindow {
 public:
     NESWindowSFML(const unsigned int& window_width = NES_WINDOW_WIDTH, const unsigned int& window_height = NES_WINDOW_HEIGHT, const std::string& window_title = NES_WINDOW_TITLE);
+    ~NESWindowSFML() override;
     void setPixel(const uint16_t& x, const uint16_t& y, const uint8_t& r, const uint8_t& g, const uint8_t& b) override;
     sf::RenderWindow& getWindow();
 private:
--- src/nes-window-sfml.cpp
+++ src/nes-window-sfml.cpp
@@ -3,6 +3,10 @@
 NESWindowSFML::NESWindowSFML(const unsigned int& window_width, const unsigned int& window_height, const std::string& window_title): 
     window_(sf::VideoMode({window_width, window_height}), window_title), pixel_({1, 1}) {}
 
+NESWindowSFML::~NESWindowSFML() {
+    window_.close();
+}
+
 void NESWindowSFML::setPixel(const uint16_t& x, const uint16_t& y, const uint8_t& r, const uint8_t& g, const uint8_t& b) {
     pixel_.setPosition({static_cast<float>(x), static_cast<float>(y)});
     pixel_.setFillColor({r, g, b});
